1a7edd156e18c1d8f9b1fc351492a377e7904d4e
woodi <woodi.daily@gmail.com>
2021-07-13 03:07:40 +0900

    Remove Styled-Components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,27 +1,27 @@
+import React from "react";
 import type { AppProps } from "next/app";
-import { ThemeProvider } from "styled-components";
 import Head from "next/head";
-//do not change the priority of scss
-import "/public/sass/noscript.scss";
-import "/public/sass/main.scss";
 
-import { GlobalStyle } from "@src/styles/global-styles";
-import { theme } from "@src/styles/theme";
+import "@styles/globals.scss";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps): JSX.Element {
 	return (
-		<ThemeProvider theme={theme}>
-			<GlobalStyle />
+		<>
 			<Head>
-				<title>boundary</title>
-				<link rel="icon" href="images/favicon.ico" />
+				<title>template</title>
+				<link rel="icon" href="/images/favicon.ico" />
 				<meta charSet="utf-8" />
+				<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+				<meta
+					httpEquiv="Content-Security-Policy"
+					content="upgrade-insecure-requests"
+				></meta>
 				<meta
 					name="viewport"
 					content="width=device-width, initial-scale=1, user-scalable=no"
 				/>
 			</Head>
 			<Component {...pageProps} />
-		</ThemeProvider>
+		</>
 	);
 }
