3761365913842e9e93f2483b0f0a4216f1b76d86
woodi <woodi.daily@gmail.com>
2022-02-04 01:29:48 +0900

    Add ParamSerializer for Axios

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Head from 'next/head'
 import axios from 'axios'
+import qs from 'qs'
 import type { AppProps } from 'next/app'
 // external module css
 import 'mapbox-gl/dist/mapbox-gl.css'
@@ -15,6 +16,9 @@ import { Composer } from '@components/common'
 
 axios.defaults.withCredentials = Boolean(process.env.NEXT_PUBLIC_API_WITH_CRED === 'true')
 axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_BASE_URL
+axios.defaults.paramsSerializer = (params) => {
+  return qs.stringify(params)
+}
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
