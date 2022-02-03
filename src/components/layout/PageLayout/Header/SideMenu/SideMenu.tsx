a979a881fda875940f2e1608f60bd7a5f2d9ce59
woodi <woodi.daily@gmail.com>
2022-02-03 13:34:38 +0900

    Resolve SideMenu Visibility Issue

--- src/components/layout/PageLayout/Header/SideMenu/SideMenu.tsx
+++ src/components/layout/PageLayout/Header/SideMenu/SideMenu.tsx
@@ -1,24 +1,16 @@
-import React, { FC, FocusEventHandler, useCallback, useRef } from 'react'
+import React, { FC, useRef } from 'react'
 import { framerSidebar } from '@utils/framerVar'
 import { motion, useCycle } from 'framer-motion'
 import { useDimensions } from '@hooks/useDimensions'
 import SideNavigation from './SideMenuNav'
 import SideMenuToggle from './SideMenuToggle'
 import styles from './SideMenu.module.scss'
+import classNames from 'classnames'
 
 const SideMenu: FC = () => {
   const [isOpen, toggleOpen] = useCycle(false, true)
   const containerRef = useRef<HTMLElement>(null)
   const { height } = useDimensions(containerRef)
-
-  const handleBlur: FocusEventHandler = useCallback(
-    (e) => {
-      if (!e.currentTarget.contains(e.relatedTarget as Node)) {
-        toggleOpen()
-      }
-    },
-    [toggleOpen]
-  )
 
   return (
     <motion.nav
@@ -26,8 +18,7 @@ const SideMenu: FC = () => {
       animate={isOpen ? 'open' : 'closed'}
       custom={height}
       ref={containerRef}
-      className={styles.container}
-      onBlur={handleBlur}
+      className={classNames(styles.container, { [styles.active]: isOpen })}
     >
       <motion.div className={styles.background} variants={framerSidebar} />
       <SideNavigation />
