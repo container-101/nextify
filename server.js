d01072bc5b8c2bb12534121cfc10f7b180325a88
woodi <woodi.daily@gmail.com>
2022-02-03 13:35:51 +0900

    Mod Nextjs Server Running by express

---
+++ server.js
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const http = require('http')
+const next = require('next')
+const port = parseInt(process.env.PORT, 10) || 3000
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+const express = require('express')
+
+app.prepare().then(() => {
+  const app = express()
+  const server = http.createServer(app)
+
+  app.all('*', (req, res) => {
+    return handle(req, res)
+  })
+
+  server.listen(port, () => {
+    console.log(`> Ready on ${process.env.PORT}`)
+  })
+})
